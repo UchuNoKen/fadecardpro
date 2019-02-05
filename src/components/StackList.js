968ecc2b5ae39cdb09d526f47374b5509fb7266a
UchuNoKen <xen.o.fite@gmail.com>
2019-02-05 18:39:46 -0500

    Added routing for Stack.

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import stacks from "../data/stacks.json";
 
 class StackList extends Component {
@@ -6,7 +7,11 @@ class StackList extends Component {
     return (
       <div>
         {stacks.map(stack => {
-          return <h4 key={stack.id}>{stack.title}</h4>;
+          return (
+            <Link to="/stack">
+              <h4 key={stack.id}>{stack.title}</h4>
+            </Link>
+          );
         })}
       </div>
     );
